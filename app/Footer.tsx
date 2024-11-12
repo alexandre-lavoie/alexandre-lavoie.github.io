c53d1f1ed088e426c43492edab28477b1eff099c
Alexandre Lavoie <alexandre.lavoie00@gmail.com>
2024-11-12 06:02:00 -0500

    Private deploy

--- app/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="p-4">
-            <div className="grid justify-start items-center"></div>
-            <div className="grid justify-center items-center"></div>
-            <div className="grid justify-end items-center"></div>
-        </footer>
-    );
-}
